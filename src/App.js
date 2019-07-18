ebe8ee8f1cdc5b3bca20f8cac44793a96453bd7c
Varma Bhupatiraju <varmab@gmail.com>
2019-07-18 13:13:14 +0530

    adding todos

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './Welcome'
+import Counter from './Counter'
+import Todos from './Todos'
+
+class  App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Welcome name="Varma" year={2020} password="12334324"/>
+          <Welcome name="Ram" year={2025} password="3423432"/>
+        </header>
+        <Todos/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/Todos.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react'
+
+class Todos extends Component{
+    constructor(){
+        super();
+
+        this.state={
+            todos:[],
+            todo:''
+        }
+    }
+
+    onTodoChange=(event)=>{
+        var currentTodo=event.target.value;
+        this.setState({
+            todo:currentTodo
+        })
+    }
+
+    onAddTodo=()=>{
+        this.setState({
+            todos:[
+                ...this.state.todos,
+                this.state.todo
+            ],
+            todo:''
+        })
+    }
+
+    removeTodo(todo){
+        this.setState({
+            todos:this.state.todos.filter((currentTodo)=>{
+                return currentTodo!=todo;
+            })
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Todos</h1>
+                <input type="text" value={this.state.todo} onChange={this.onTodoChange} name="todo"/>
+                <button onClick={this.onAddTodo}>Add</button>
+                <ul>
+                {
+                    this.state.todos.map((todo,index)=>{
+                        return <li key={index}>{todo} <button onClick={
+                            ()=>{
+                                this.removeTodo(todo);
+                            }
+                        }>Delete</button></li>
+                    })
+                }
+                </ul>
+
+            </div>
+        )
+    }
+}
+ export default Todos;
